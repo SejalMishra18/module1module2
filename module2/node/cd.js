28c0313abd701760e4c09e8b059bea222ad37667
SejalMishra18 <sejalmishra2000@gmail.com>
2022-03-13 15:15:36 +0530

    directory related

--- module2/node/cd.js
+++ module2/node/cd.js
@@ -1,12 +1,21 @@
 let fs=require("fs");
 let path=require("path");
 //create a directory.....
-//if(!fs.existsSync("owndirectory"))
-//fs.mkdirSync("owndirectory");
+if(!fs.existsSync("owndirectory"))
+fs.mkdirSync("owndirectory");
 //read a directory...
-//let folderpath=__dirname;
+//let folderpath="__dirname"
 //let content=fs.readFileSync(folderpath,'utf-8');
-console.log(__dirname);
+//console.log(content);
+//to copyfiletxt and put into module folder
+let source=path.join(__dirname,"file.txt") ;
+let destination=path.join(__dirname,"module","file.txt");
+fs.copyFileSync(source,destination);
+//to delete directory
+fs.rmdirSync("owndirectory");
+
+
+
 
 
 
