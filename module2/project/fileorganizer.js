cebd7fbf8cc4a904261e49116c9315a8fb140a3d
SejalMishra18 <sejalmishra2000@gmail.com>
2022-03-18 23:59:01 +0530

    fileorganizerproject

--- module2/project/fileorganizer.js
+++ module2/project/fileorganizer.js
@@ -1,4 +1,64 @@
-let inputarray=process.argv;
+//let inputarray=process.argv;
 //console.log(inputarray);
-let input=process.argv[4];
-console.log(input);
+//let input=process.argv[2];
+//console.log(input);
+let fs=require("fs");
+let path=require("path");
+let folderpath=process.argv[2];//take an input 
+//console.log(folderpath);
+let extensions={
+    Audio:[".mp3"],
+    Video:[".mp4",".mkv"],
+    Images:[".jpg",".jpeg",".gif",".png"],
+    Documents:[".pdf",".doc",'.xlsx'],
+    Software:[".exe"]
+};
+let folderexist=fs.existsSync(folderpath);
+if(folderexist)
+{
+    let files=fs.readdirSync(folderpath);
+    for(let i=0;i<files.length;i++)
+    {
+        let ext=path.extname(files[i]);
+       // console.log(ext);
+       let foldername= givefoldername(ext);
+      // console.log("Ext-",ext,"Folder-",foldername);
+      let pathoffolder=path.join(folderpath,foldername);
+      let exist=fs.existsSync(pathoffolder);
+      if(exist)
+      {
+       movefile(folderpath,pathoffolder,files[i]);
+      }
+      else{
+          fs.mkdirSync(pathoffolder);
+          movefile(folderpath,pathoffolder,files[i]);
+      }
+
+
+       }
+}
+else{
+    console.log("Enter a valid path!!!");
+}
+function givefoldername(ext)
+{
+    for(let key in extensions)
+    {
+        let extarr=extensions[key];
+        for(let i=0;i<extarr.length;i++)
+        {
+            if(extarr[i]==ext)
+            {
+                return key;
+            }
+        }
+    }
+    return 'Others';
+}
+function movefile(folderpath,pathoffolder,filename)
+{
+    let srcpath=path.join(folderpath,filename);
+    let destpath=path.join(pathoffolder,filename);
+    fs.copyFileSync(srcpath,destpath);
+    fs.unlinkSync(srcpath);
+}
